6e042a68279a2080d7d3003782cd12eddf9e6bab
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2025-04-01 20:43:20 -0700

    lifted logout state to make floating button work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,69 @@
 // import Analysis from "./Analysis";
-import './App.css';
+import "./App.css";
 import Analysis from "./Analysis";
 import UserAuth from "./UserAuth";
 import axios from "axios";
 
 import React, { useState } from "react";
 
+const baseURL = "http://127.0.0.1:8000/";
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
+axios.defaults.withCredentials = true;
+
+const getCookie = (name) => {
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(name + "="))
+    ?.split("=")[1];
+  return cookieValue;
+};
+
+const csrfToken = getCookie("csrftoken");
+
+const client = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "X-CSRFToken": csrfToken,
+  },
+});
+
+const fetchCsrfToken = async () => {
+  try {
+    await axios.get(`${baseURL}api/csrf/`);
+    console.log("CSRF token set successfully");
+  } catch (error) {
+    console.error(
+      "Error setting CSRF token:",
+      error.response?.data || error.message
+    );
+  }
+};
+
 const App = () => {
   const [registrationToggle, setRegistrationToggle] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
+  // const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   // console.log("is logged in: ", isLoggedIn);
+
+  function submitLogout(e) {
+    // e.preventDefault();
+    client.post("/logout/", { withCredentials: true }).then(function (res) {
+      setCurrentUser(false);
+      setEmail("");
+      setUsername("");
+    });
+  }
 
   const handleButtonClick = () => {
     if (isLoggedIn) {
       // Handle logout
       setIsLoggedIn(false);
+      submitLogout();
     } else {
       // Toggle between login and registration
       setRegistrationToggle(!registrationToggle);
@@ -32,7 +81,7 @@ const App = () => {
       <button className="floating-button" onClick={handleButtonClick}>
         {getButtonLabel()}
       </button>
-       {isLoggedIn ? (
+      {isLoggedIn ? (
         // Show the main app if the user is logged in
         <Analysis />
       ) : (
@@ -41,10 +90,18 @@ const App = () => {
           registrationToggle={registrationToggle}
           setRegistrationToggle={setRegistrationToggle}
           setIsLoggedIn={setIsLoggedIn} // Pass login state updater
+          fetchCsrfToken={fetchCsrfToken}
+          client={client}
+          currentUser={currentUser}
+          setCurrentUser={setCurrentUser}
+          email={email}
+          setEmail={setEmail}
+          username={username}
+          setUsername={setUsername}
         />
       )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- frontend/src/UserAuth.js
+++ frontend/src/UserAuth.js
@@ -1,65 +1,78 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 axios.defaults.withCredentials = true;
 
 // const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]')?.value;
 
-const getCookie = (name) => {
-  const cookieValue = document.cookie
-      .split('; ')
-      .find(row => row.startsWith(name + '='))
-      ?.split('=')[1];
-  return cookieValue;
-};
-const csrfToken = getCookie('csrftoken');
+// const getCookie = (name) => {
+//   const cookieValue = document.cookie
+//       .split('; ')
+//       .find(row => row.startsWith(name + '='))
+//       ?.split('=')[1];
+//   return cookieValue;
+// };
+// const csrfToken = getCookie('csrftoken');
 
-const baseURL = "http://127.0.0.1:8000/";
+// const baseURL = "http://127.0.0.1:8000/";
 
-const client = axios.create({
-  baseURL: baseURL,
-  headers: {
-    'X-CSRFToken': csrfToken
-  }
-})
+// const client = axios.create({
+//   baseURL: baseURL,
+//   headers: {
+//     'X-CSRFToken': csrfToken
+//   }
+// })
 
-const fetchCsrfToken = async () => {
-  try {
-    await axios.get(`${baseURL}api/csrf/`);
-    console.log("CSRF token set successfully");
-  } catch (error) {
-    console.error("Error setting CSRF token:", error.response?.data || error.message);
-  }
-};
+// const fetchCsrfToken = async () => {
+//   try {
+//     await axios.get(`${baseURL}api/csrf/`);
+//     console.log("CSRF token set successfully");
+//   } catch (error) {
+//     console.error("Error setting CSRF token:", error.response?.data || error.message);
+//   }
+// };
 
-const UserAuth = ({ registrationToggle, setRegistrationToggle, setIsLoggedIn }) => {
-  const [currentUser, setCurrentUser] = useState();
-  // const [registrationToggle, setRegistrationToggle] = useState(false);
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const UserAuth = ({
+  registrationToggle,
+  setRegistrationToggle,
+  setIsLoggedIn,
+  fetchCsrfToken,
+  client,
+  currentUser,
+  setCurrentUser,
+  email,
+  setEmail,
+  username,
+  setUsername,
+}) => {
+  // const [currentUser, setCurrentUser] = useState();
+  // // const [registrationToggle, setRegistrationToggle] = useState(false);
+  // const [email, setEmail] = useState("");
+  // const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     fetchCsrfToken();
-    client.get("/user/")
-    .then(function(res) {
-      // console.log("user response: ", res);
-      setCurrentUser(true);
-      setEmail(res.data.user.email);
-      setUsername(res.data.user.username);
-      // console.log(res);
-    })
-    .catch(function(error) {
-      setCurrentUser(false);
-    });
+    client
+      .get("/user/")
+      .then(function (res) {
+        // console.log("user response: ", res);
+        setCurrentUser(true);
+        setEmail(res.data.user.email);
+        setUsername(res.data.user.username);
+        // console.log(res);
+      })
+      .catch(function (error) {
+        setCurrentUser(false);
+      });
   }, []);
 
   function update_form_btn() {
@@ -74,57 +87,51 @@ const UserAuth = ({ registrationToggle, setRegistrationToggle, setIsLoggedIn }) 
 
   function submitRegistration(e) {
     e.preventDefault();
-    client.post(
-      "/register/",
-      {
+    client
+      .post("/register/", {
         email: email,
         username: username,
-        password: password
-      }
-    ).then(function(res) {
-      client.post(
-        "/login/",
-        {
-          email: email,
-          password: password
-        }
-      ).then(function(res) {
+        password: password,
+      })
+      .then(function (res) {
+        client
+          .post("/login/", {
+            email: email,
+            password: password,
+          })
+          .then(function (res) {
+            setCurrentUser(true);
+            setEmail(email);
+            setUsername(username);
+            setIsLoggedIn(true);
+          });
+      });
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
+    client
+      .post("/login/", {
+        email: email,
+        password: password,
+      })
+      .then(function (res) {
+        console.log(res);
         setCurrentUser(true);
         setEmail(email);
         setUsername(username);
         setIsLoggedIn(true);
       });
-    });
   }
 
-  function submitLogin(e) {
-    e.preventDefault();
-    client.post(
-      "/login/",
-      {
-        email: email,
-        password: password
-      }
-    ).then(function(res) {
-      console.log(res);
-      setCurrentUser(true);
-      setEmail(email);
-      setUsername(username);
-      setIsLoggedIn(true);
-    });
-  }
-
-  function submitLogout(e) {
-    e.preventDefault();
-    client.post(
-      "/logout/",
-      {withCredentials: true}
-    ).then(function(res) {
-      setCurrentUser(false);
-      setEmail("");
-      setUsername("");
-    });
-  }
+  // function submitLogout(e) {
+  //   e.preventDefault();
+  //   client.post("/logout/", { withCredentials: true }).then(function (res) {
+  //     setCurrentUser(false);
+  //     setEmail("");
+  //     setUsername("");
+  //   });
+  // }
 
   if (currentUser) {
     return (
@@ -140,74 +147,86 @@ const UserAuth = ({ registrationToggle, setRegistrationToggle, setIsLoggedIn }) 
                   <h3>email: {email}</h3>
                   <h3>username: {username}</h3>
                 </div>
-                <form onSubmit={(e) => submitLogout(e)}>
-                  <Button type="submit" variant="light">Log out</Button>
-                </form>
+                {/* <form onSubmit={(e) => submitLogout(e)}>
+                  <Button type="submit" variant="light">
+                    Log out
+                  </Button>
+                </form> */}
               </div>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        </div>
+      </div>
     );
   }
   return (
     <div>
-    {
-      registrationToggle ? (
+      {registrationToggle ? (
         <div className="center">
-          <Form onSubmit={e => submitRegistration(e)}>
+          <Form onSubmit={(e) => submitRegistration(e)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-              </Form.Text>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>        
-      ) : (
-        <div className="center">
-          <Form onSubmit={e => submitLogin(e)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
             </Button>
           </Form>
         </div>
-      )
-    }
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        {/* <Navbar.Brand>Authentication App</Navbar.Brand> */}
-        {/* <Navbar.Toggle /> */}
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <Button id="form_btn" onClick={update_form_btn} variant="light">Register</Button>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      ) : (
+        <div className="center">
+          <Form onSubmit={(e) => submitLogin(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default UserAuth;
