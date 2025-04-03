d93e5550137ff02b8c7b2409c989a49b4e6810a5
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2025-04-03 12:45:33 -0700

    more frontend styling

--- frontend/src/Analysis.js
+++ frontend/src/Analysis.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState, useContext, memo } from "react";
 import { format } from "date-fns";
-// import "./App.css";
+import "./App.css";
 import { Line } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
-import 'chartjs-adapter-moment';
-import styles from "./App.module.css"
-import UserAuth from './UserAuth';
-
+import Chart from "chart.js/auto";
+import "chartjs-adapter-moment";
+// import styles from "./App.module.css";
+import UserAuth from "./UserAuth";
 
 const baseUrl = "http://127.0.0.1:8000/api";
 
@@ -23,37 +22,39 @@ const Analysis = memo(() => {
 
   // const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]')?.value;
 
-const getCookie = (name) => {
+  const getCookie = (name) => {
     const cookieValueRaw = document.cookie;
     // console.log("raw cookies are: ", cookieValueRaw);
     const cookieValue = cookieValueRaw
-        .split('; ')
-        .find(row => row.startsWith(name + '='))
-        ?.split('=')[1];
+      .split("; ")
+      .find((row) => row.startsWith(name + "="))
+      ?.split("=")[1];
     // console.log("cookie ", name, " is ", cookieValue);
     return cookieValue;
-};
-const csrfToken = getCookie('csrftoken');
+  };
+  const csrfToken = getCookie("csrftoken");
 
-// console.log("\n\n\nCSRF TOKEN: ", csrfToken, "\n\n\n");
+  // console.log("\n\n\nCSRF TOKEN: ", csrfToken, "\n\n\n");
 
   // const csrfToken = "RFH6HfFcmQc8HqifyH9rT8QwwNFdS6XzoNLa5asPgoFNwKmcOuo5Vy6Q9y3gwGNA";
 
   const fetchEvents = async () => {
     // console.log("fetching events");
-    const csrfToken = getCookie('csrftoken');
-    const resp = await axios.get(`${baseUrl}/entries/`, {
-      headers: {
-        'X-CSRFToken': csrfToken
-      }
-    }
-    ).then(resp => {
-      const events = resp.data;
-      // console.log("events: ", events, typeof(events));
-      setEventsList(events);
-    }).catch(err => {
-      console.log(err.response.data.error)
-    });
+    const csrfToken = getCookie("csrftoken");
+    const resp = await axios
+      .get(`${baseUrl}/entries/`, {
+        headers: {
+          "X-CSRFToken": csrfToken,
+        },
+      })
+      .then((resp) => {
+        const events = resp.data;
+        // console.log("events: ", events, typeof(events));
+        setEventsList(events);
+      })
+      .catch((err) => {
+        console.log(err.response.data.error);
+      });
     // console.log("data: ", resp.data, typeof(resp.data));
     // const events = resp.data;
     // console.log("events: ", events, typeof(events));
@@ -73,11 +74,11 @@ const csrfToken = getCookie('csrftoken');
   const handleDelete = async (event) => {
     const id = event.id;
     try {
-      const csrfToken = getCookie('csrftoken');
+      const csrfToken = getCookie("csrftoken");
       await axios.delete(`${baseUrl}/entries/${id}/`, {
         withCredentials: true,
         headers: {
-          'X-CSRFToken': csrfToken,
+          "X-CSRFToken": csrfToken,
         },
       });
       const updatedList = eventsList.filter((event) => event.id !== id);
@@ -97,15 +98,15 @@ const csrfToken = getCookie('csrftoken');
     e.preventDefault(); // Prevents page from refreshing
     try {
       const url = `${baseUrl}/entries/${eventIdBeingEdited}/`;
-      const csrfToken = getCookie('csrftoken');
+      const csrfToken = getCookie("csrftoken");
       const response = await axios.put(
         url,
         { description: editDescription }, // Updated description
         {
           withCredentials: true,
           headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken,
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrfToken,
           },
         }
       );
@@ -131,20 +132,22 @@ const csrfToken = getCookie('csrftoken');
     try {
       // console.log("DOING SUBMIT!");
       // console.log(`${baseUrl}/entries/`, { description });
-      const csrfToken = getCookie('csrftoken');
+      const csrfToken = getCookie("csrftoken");
       // console.log("Doing POST, csrftoken is ", csrfToken);
-      const data = await axios.post(`${baseUrl}/entries/`, 
+      const data = await axios.post(
+        `${baseUrl}/entries/`,
         {
-          "description": description,  // Data payload
-        },  
+          description: description, // Data payload
+        },
         {
-          withCredentials: true,  
+          withCredentials: true,
           headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrfToken,
           },
           // credentials: 'include',
-        });
+        }
+      );
       // setEventsList([...eventsList, data.data]);
       fetchEvents();
       setDescription("");
@@ -157,57 +160,87 @@ const csrfToken = getCookie('csrftoken');
     fetchEvents();
   }, []);
 
-  const emotions = ["admiration", "amusement", "anger", "annoyance", "approval", "caring", "confusion", "curiosity", "desire", "disappointment", "disapproval", "disgust", "embarrassment", "excitement", "fear", "gratitude", "grief", "joy", "love", "nervousness", "optimism", "pride", "realization", "relief", "remorse", "sadness", "surprise"];
+  const emotions = [
+    "admiration",
+    "amusement",
+    "anger",
+    "annoyance",
+    "approval",
+    "caring",
+    "confusion",
+    "curiosity",
+    "desire",
+    "disappointment",
+    "disapproval",
+    "disgust",
+    "embarrassment",
+    "excitement",
+    "fear",
+    "gratitude",
+    "grief",
+    "joy",
+    "love",
+    "nervousness",
+    "optimism",
+    "pride",
+    "realization",
+    "relief",
+    "remorse",
+    "sadness",
+    "surprise",
+  ];
   const default_emotions_dict = {};
-  emotions.map(emotion => default_emotions_dict[emotion] = true);
+  emotions.map((emotion) => (default_emotions_dict[emotion] = true));
   const [checked, setChecked] = React.useState(default_emotions_dict);
 
-  const checked_emotions = emotions.filter(emotion => checked[emotion]);
-  
-  const rbgaClear = "rgba(0,0,0,0)"
+  const checked_emotions = emotions.filter((emotion) => checked[emotion]);
+
+  const rbgaClear = "rgba(0,0,0,0)";
 
   // TODO: get correct data for each emotion
-  const datasets = checked_emotions.map(emotion =>
-      ({
-        label: emotion,
-        data: eventsList.map(event => event.emotions[0][emotions.indexOf(emotion)].score),
-        fill: true,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)",
-      }));
+  const datasets = checked_emotions.map((emotion) => ({
+    label: emotion,
+    data: eventsList.map(
+      (event) => event.emotions[0][emotions.indexOf(emotion)].score
+    ),
+    fill: true,
+    backgroundColor: "rgba(75,192,192,0.2)",
+    borderColor: "rgba(75,192,192,1)",
+  }));
 
   datasets.push({
     label: "Composite Sentiment",
-    data: eventsList.map(event => event.sentiment),
+    data: eventsList.map((event) => event.sentiment),
     fill: true,
     backgroundColor: rbgaClear,
     borderColor: "rgba(200,192,192,1)",
-    visible: true
-  })
+    visible: true,
+  });
 
   datasets.push({
     label: "TextBlob Polarity",
-    data: eventsList.map(event => event.blob_sentiment[0]),
+    data: eventsList.map((event) => event.blob_sentiment[0]),
     fill: true,
     backgroundColor: rbgaClear,
     borderColor: "rgba(19, 157, 180, 0.8)",
-    visible: true
-  })
+    visible: true,
+  });
 
   datasets.push({
     label: "TextBlob Subjectivity",
-    data: eventsList.map(event => event.blob_sentiment[1]),
+    data: eventsList.map((event) => event.blob_sentiment[1]),
     fill: true,
     backgroundColor: rbgaClear,
     borderColor: "rgba(245, 40, 145, 0.8)",
-    visible: true
-  })
-                                  
-  
-  const handleCheckboxChange = (emotion) => 
-  {
-    setChecked(prevChecked => ({...prevChecked, [emotion]: !prevChecked[emotion]}));
-  }
+    visible: true,
+  });
+
+  const handleCheckboxChange = (emotion) => {
+    setChecked((prevChecked) => ({
+      ...prevChecked,
+      [emotion]: !prevChecked[emotion],
+    }));
+  };
 
   const eventsData = {
     labels: eventsList.map((event) => event.created_at),
@@ -219,99 +252,123 @@ const csrfToken = getCookie('csrftoken');
     plugins: {
       title: {
         display: true,
-        text: 'Analysis'
-      }
+        text: "Analysis",
+        font: {
+          size: 24, // Increase the font size of the title
+          weight: "bold", // Make the title bold
+        },
+        padding: {
+          top: 20, // Add vertical space above the title
+          bottom: 30, // Add vertical space between the title and the plot
+        },
+      },
+      legend: {
+        display: true,
+        position: "left", // Move all labels to the left side
+        align: "start", // Align labels to the top of the column
+        labels: {
+          boxWidth: 20, // Size of the legend box
+          font: {
+            size: 12, // Font size for labels
+          },
+          padding: 10, // Spacing between labels
+        },
+      },
+    },
+    layout: {
+      padding: {
+        left: 20, // Add padding to the left
+        right: 50, // Add more padding to the right
+        top: 10, // Add padding to the top
+        bottom: 10, // Add padding to the bottom
+      },
     },
     scales: {
       x: {
-        type: 'time',
+        type: "time",
         time: {
-            unit: 'hour', // Change to 'minute', 'hour', etc. as needed
-            tooltipFormat: 'll HH:mm', // Format for tooltip display
-            displayFormats: {
-                day: 'MMM D', // Format for day-level labels
-                hour: 'MMM D, HH:mm' // Format for hour-level labels
-            }
+          unit: "hour", // Change to 'minute', 'hour', etc. as needed
+          tooltipFormat: "ll HH:mm", // Format for tooltip display
+          displayFormats: {
+            day: "MMM D", // Format for day-level labels
+            hour: "MMM D, HH:mm", // Format for hour-level labels
+          },
         },
         title: {
-            display: true,
-            text: 'Date and Time'
+          display: true,
+          text: "Date and Time",
         },
         ticks: {
-            // Customize tick labels if needed
-            callback: function(value) {
-                const date = new Date(value);
-                return date.toLocaleString(); // Custom format using toLocaleString() if needed
-            }
-        }
-    },
+          // Customize tick labels if needed
+          callback: function (value) {
+            const date = new Date(value);
+            return date.toLocaleString(); // Custom format using toLocaleString() if needed
+          },
+        },
+      },
       y: {
         display: true,
         // beginAtZero: true,
         // type: 'logarithmic',
-      }
-    }
+      },
+    },
   };
 
-
   return (
-    <><h2>How are you doing today? What happened? How did that make you feel?</h2><section>
+    <>
+      <h2>
+        How are you doing today? What happened? How did that make you feel?
+      </h2>
+      <section>
         <form onSubmit={handleSubmit} className={"center"}>
           <label htmlFor="description"></label>
           <textarea
-            className={styles.text_box}
+            // className={text_box}
             onChange={(e) => handleChange(e, "description")}
             type="text"
             name="description"
             id="description"
-            value={description} />
+            value={description}
+          />
           <button type="Submit">Submit</button>
         </form>
-      </section><Line data={eventsData}
-        options={chartOptions} /><div className={styles.emotions_checkboxes}>
-          {emotions.map((emotion) => <div>
-            <input type="checkbox" key={emotion}
-              checked={checked[emotion]}
-              onChange={() => handleCheckboxChange(emotion)}
-            >
-            </input>
-            <div>{emotion}</div>
-            {/* <div>{checked[emotion].toString()}</div> */}
-          </div>)}
-        </div><section>
-          <ul>
-            {eventsList.map((event) => {
-              return (
-                <li key={event.id}>
-                  {eventIdBeingEdited === event.id ? (
-                    <form onSubmit={handleEditSubmit} key={event.id}>
-                      <input
-                        onChange={(e) => handleEditChange(e, "edit")}
-                        type="text"
-                        name="editDescription"
-                        id="editDescription"
-                        value={editDescription}
-                      ></input>
-                      <button type="Submit">Finish Editing</button>
-                      <button type="button" onClick={handleCancelEdit}>
-                        Cancel
-                      </button>
-                    </form>
-                  ) : (
-                    <div>
-                      <button onClick={() => handleDelete(event)}>X</button>
-                      <button onClick={() => handleEdit(event)}>Edit</button>
-                      {format(new Date(event.created_at), "MM/dd/yyyy, p")}:{" "}
-                      <br />
-                      {event.description}
-                    </div>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-        </section></>
-  )
-})
+      </section>
+      <Line data={eventsData} options={chartOptions} />
+      <section>
+        <ul>
+          {eventsList.map((event) => {
+            return (
+              <li key={event.id}>
+                {eventIdBeingEdited === event.id ? (
+                  <form onSubmit={handleEditSubmit} key={event.id}>
+                    <input
+                      onChange={(e) => handleEditChange(e, "edit")}
+                      type="text"
+                      name="editDescription"
+                      id="editDescription"
+                      value={editDescription}
+                    ></input>
+                    <button type="Submit">Finish Editing</button>
+                    <button type="button" onClick={handleCancelEdit}>
+                      Cancel
+                    </button>
+                  </form>
+                ) : (
+                  <div>
+                    <button onClick={() => handleDelete(event)}>X</button>
+                    <button onClick={() => handleEdit(event)}>Edit</button>
+                    {format(new Date(event.created_at), "MM/dd/yyyy, p")}:{" "}
+                    <br />
+                    {event.description}
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
+});
 
 export default Analysis;
