53b3aa39bdac8253173f60fafb34314209cebd8a
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2025-04-17 17:23:13 -0700

    adds nltk setup stuff

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Download NLTK data
+python nltk_setup.py
+
 python manage.py collectstatic --no-input
 python manage.py migrate
---
+++ backend/nltk_setup.py
@@ -0,0 +1,7 @@
+import nltk
+
+# Download required NLTK resources
+nltk.download('vader_lexicon')
+# Add any other NLTK resources you might need
+# nltk.download('punkt')
+# nltk.download('stopwords')
